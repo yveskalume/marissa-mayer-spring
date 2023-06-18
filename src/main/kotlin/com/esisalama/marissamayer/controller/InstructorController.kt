9fdf96b1d105c50881b43b38b83611b5d501eeb8
yveskalume <yvkalume@gmail.com>
2023-06-18 16:26:03 +0200

    instructor courses

---
+++ src/main/kotlin/com/esisalama/marissamayer/controller/InstructorController.kt
@@ -0,0 +1,26 @@
+package com.esisalama.marissamayer.controller
+
+import com.esisalama.marissamayer.config.CustomUserService
+import com.esisalama.marissamayer.data.entity.Utilisateur
+import com.esisalama.marissamayer.data.repository.CoursRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.User
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class InstructorController(
+        @Autowired private val coursRepository: CoursRepository
+) {
+
+    @GetMapping(value = ["/instructor/my-courses"], name = "instructor.my-courses")
+    fun myCourses(auth:Authentication,model: Model): String {
+        val user = auth.principal as User
+        println(user.username)
+        val courses = user.username?.let { coursRepository.findByInstructeurEmail(it) } ?: listOf()
+        model.addAttribute("courses", courses)
+        return "instructor/my_courses"
+    }
+}
