fda17fca1d4c69a2375e4dad1d5294f86418c58a
yveskalume <yvkalume@gmail.com>
2023-06-21 02:04:38 +0200

    new logo

--- src/main/kotlin/com/esisalama/marissamayer/controller/HomeController.kt
+++ src/main/kotlin/com/esisalama/marissamayer/controller/HomeController.kt
@@ -5,7 +5,6 @@ import jakarta.servlet.RequestDispatcher
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.servlet.error.ErrorController
-import org.springframework.http.HttpStatus
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -25,12 +24,12 @@ class HomeController(
     }
 
     @RequestMapping("/error")
-    fun handleError(request: HttpServletRequest,model: Model): String? {
+    fun handleError(request: HttpServletRequest, model: Model): String? {
         val status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)
         if (status != null) {
-            model.addAttribute("statusCode",status)
+            model.addAttribute("statusCode", status)
         } else {
-            model.addAttribute("statusCode","404")
+            model.addAttribute("statusCode", "404")
         }
         return "error"
     }
